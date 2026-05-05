165c906dafa67ae497598e4a12bfcd6860f79862
LuizZonetti1 <luizfbzonetti@gmail.com>
2026-05-05 16:17:31 -0300

    ajustes pequenos de formatação

--- src/components/QuickActions/index.tsx
+++ src/components/QuickActions/index.tsx
@@ -4,35 +4,35 @@ import { QuickAccessCard } from "../QuickAccessCard";
 import { QuickActionsGrid, QuickActionsSection, QuickActionsTitle } from "./styles";
 
 export interface QuickAction {
-  icon: ReactNode;
-  label: string;
-  color: string;
-  path: string;
+    icon: ReactNode;
+    label: string;
+    color: string;
+    path: string;
 }
 
 interface QuickActionsProps {
-  actions: QuickAction[];
-  title?: string;
+    actions: QuickAction[];
+    title?: string;
 }
 
 export const QuickActions = ({ actions, title = "Acesso Rápido" }: QuickActionsProps) => {
-  const navigate = useNavigate();
-  return (
-    <QuickActionsSection>
-      <QuickActionsTitle>{title}</QuickActionsTitle>
-      <QuickActionsGrid>
-        {actions.map((action) => (
-          <QuickAccessCard
-            key={action.path}
-            icon={action.icon}
-            label={action.label}
-            color={action.color}
-            onClick={() => navigate(action.path)}
-          />
-        ))}
-      </QuickActionsGrid>
-    </QuickActionsSection>
-  );
+    const navigate = useNavigate();
+    return (
+        <QuickActionsSection>
+            <QuickActionsTitle>{title}</QuickActionsTitle>
+            <QuickActionsGrid>
+                {actions.map((action) => (
+                    <QuickAccessCard
+                        key={action.path}
+                        icon={action.icon}
+                        label={action.label}
+                        color={action.color}
+                        onClick={() => navigate(action.path)}
+                    />
+                ))}
+            </QuickActionsGrid>
+        </QuickActionsSection>
+    );
 };
 
 export default QuickActions;
